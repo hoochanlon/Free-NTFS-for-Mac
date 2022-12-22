a15de0f7a021fb117a5c455fcdaa761f9e65ca37
菠萝包与蛋奶酥 <hoochanlon@outlook.com>
2022-12-22 17:39:02 +0800

    Add files via upload

---
+++ py-shell/4.lunch-shell-ntfs.py
@@ -0,0 +1,24 @@
+from time import sleep
+import os
+# 第一个U盘完全挂载，再插入第二个U盘
+while True:
+    sleep(5)
+    udev = os.popen("mount | grep ntfs | awk -F ' ' '{print $1}'")
+    udev_result = udev.read()
+    udev.close()
+    if udev_result:
+        print("已接入优盘")
+        os.system("chmod 777 /Users/chanlonhoo/Desktop/4.auto.sh")
+        os.system("/Users/chanlonhoo/Desktop/4.auto.sh")
+        while True:
+            sleep(5)
+            udev = os.popen("mount | grep ntfs | awk -F ' ' '{print $1}'")
+            udev_result = udev.read()
+            udev .close()
+            if not udev_result:
+                print("------------")
+                break
+
+    else:
+        print("No USB disk found")
+        continue
