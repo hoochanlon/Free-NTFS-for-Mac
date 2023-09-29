568493a3cf44dcf86207c278d7d84d09a73041b1
Hu TsingLoong <hoochanlon@outlook.com>
2023-09-30 02:16:15 +0800

    Update nigate.sh

--- nigate.sh
+++ nigate.sh
@@ -1,6 +1,6 @@
 config_u_drive(){
 	sudo spctl --master-disable
-	
+
 	if [ ! -x $(command -v swift) ]; then
 	xcode-select --install
 	fi
@@ -14,27 +14,50 @@ config_u_drive(){
 	if [ ! -e "/System/Volumes/Data/$(which ntfs-3g)" ]; then
 	brew tap gromgit/homebrew-fuse && brew install --cask macfuse && brew install ntfs-3g-mac
 	fi
-	
-	for i in $1; do
-		onceCutVal=${i%/*}
-		twiceCutVal=${onceCutVal#*//}
-		thriceCutVal=${i##*/}
-		echo "新设备: "${thriceCutVal}
-		## echo '---------\n'
-		sudo umount $i
-		sudo -S /System/Volumes/Data/$(which ntfs-3g) /dev/${twiceCutVal} "/Volumes/${thriceCutVal}" -olocal -oallow_other -oauto_xattr -ovolname="${thriceCutVal}"
-		echo "新设备: ${thriceCutVal}，已可读写！"
-		# echo '---------\n'
-		echo '---------'
-		echo " "
-	done
 
+
+	#***********************************************************************
+	# macOS13及以下
+	# echo $i
+	# macOS14输出结果：/dev/disk6s1
+
+	# for i in $1; do
+
+	# 	onceCutVal=${i%/*}
+	# 	twiceCutVal=${onceCutVal#*//}
+	# 	thriceCutVal=${i##*/}
+	# 	echo "新设备: "${thriceCutVal}
+
+	# 	# echo "---------\n"
+	# 	sudo umount $i
+
+	# 	sudo -S /System/Volumes/Data/$(which ntfs-3g) /dev/${twiceCutVal} "/Volumes/${thriceCutVal}" -olocal -oallow_other -oauto_xattr -ovolname="${thriceCutVal}"
+	# 	echo "新设备: ${thriceCutVal}，已可读写！"
+
+	# 	echo "---------\n"
+	# done
+	#***********************************************************************
+
+
+	#***********************************************************************
+	# macOS14及以上
+	# 输出结果：
+	# /dev/disk4s1 on /Volumes/TOSHIBA (ntfs, local, nodev, nosuid, read-only, noowners, noatime)
+	line=$(mount | grep ntfs)
+
+	# 提取disk和volume名称
+	disk=$(echo "$line" | awk '{split($1, a, "/"); print a[3]}')
+	volume=$(echo "$line" | awk '{split($3, a, "/"); print a[3]}')
+
+	# echo "Disk: $disk"
+	# echo "Volume: $volume"
+	sudo umount /dev/$disk
+	sudo -S  ntfs-3g /dev/$disk  /Volumes/$volume -olocal -oallow_other -o auto_xattr -ovolname=$volume
+	echo "新设备: ${volume}，已可读写！"
+	echo '---------'
+	echo " "
+	#***********************************************************************
 }
-
-# origin echo text
-# echo "\n"
-# echo "等待NTFS新设备接入\n"
-# echo '---------\n'
 
 echo " "
 echo " "
